dbb603b7d2b6632feb735884e852b44d91abb629
Niflanijam <154239244+Niflanijam@users.noreply.github.com>
2024-08-11 00:00:56 +0530

    Delete portfolio directory

--- portfolio/javascript/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('BUTTON1').addEventListener('click', function() {
-    document.getElementById('image1').style.display = 'block';
-    document.getElementById('image2').style.display = 'none';
-});
-
-document.getElementById('BUTTON2').addEventListener('click', function() {
-    document.getElementById('image1').style.display = 'none';
-    document.getElementById('image2').style.display = 'block';
-});
